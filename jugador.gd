de6e8afa1cc2dcfde3344161030a71a3538d99f2
Inkanari <dkgames075@gmail.com>
2025-08-28 02:16:22 -0600

    inventario
    cree un tscn. del tipo canvas layer para el UI. puse un arbol mas para una segunda prueba en el pueblo y en el script del jugador agregue una tecla para poder abrir ese inventario. es la tecla: "p"

--- jugador.gd
+++ jugador.gd
@@ -1,7 +1,31 @@
 extends CharacterBody2D
 
+#inventario
+var inventario = {}
+#variables generales
 @export var speed = 120
 @export var daño: int = 1
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("UI_single"):
+		abrir_inventario()
+#inventario
+func abrir_inventario():
+	$"../../Inventario".visible = not $"../../Inventario".visible
+func agregar_item(item_name: String, amount: int = 1) -> void:
+	if inventario.has(item_name):
+		inventario[item_name] += amount
+	else:
+		inventario[item_name] = amount
+	print("inventario: ", inventario)
+func remover_item(item_name: String, amount: int = 1) -> void:
+	if inventario.has(item_name):
+		inventario[item_name] -= amount
+		if inventario[item_name] <= 0:
+			inventario.erase(item_name)
+	print("inventario: ", inventario)
+func has_item(item_name: String, amount: int = 1) -> bool:
+	return inventario.has(item_name) and inventario[item_name] >= amount
 #daño
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("golpe"):
