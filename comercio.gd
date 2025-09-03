6b540c4c56bbe676c1892d154752571a41cbdd56
Inkanari <dkgames075@gmail.com>
2025-09-03 02:10:21 -0600

    mejore el sistema de comercio/inventario
    ahora el sistema de comercio tiene precios y un orden por codigo que permite optimizarce a si mismo al menos un poco

--- comercio.gd
+++ comercio.gd
@@ -1,15 +1,28 @@
 extends GridContainer
 
 #productos
-var manzana = preload("res://icons_textura/manzana.tres")
-var lingote_oro: Texture2D = preload("res://icons_textura/lingote_oro.tres")
-
 var productos = [
-	lingote_oro,
-	manzana
+	{
+		"nombre": "lingote_de_oro",
+		"icon": preload("res://icons_textura/lingote_oro.tres"),
+		"precio": 100
+	},
+	{
+		"nombre": "manzana",
+		"icon": preload("res://icons_textura/manzana.tres"),
+		"precio": 10
+	}
 ]
 
 func _ready() -> void:
 	randomize()
-	var textura_random = productos.pick_random()
-	$casilla_1/TextureRect.texture = textura_random
+	var copia = productos.duplicate() #se crea una copia de los productos
+	copia.shuffle() #shuffle significa barajear para cambiar el orden de los productos
+	for i in range(min(get_child_count(), copia.size())):
+		var casilla = get_child(i)
+		var producto = copia[i]
+		casilla.get_node("TextureRect").texture = producto["icon"]
+		casilla.get_node("Label").text = str(producto["precio"]) + "$"
+
+func _on_casilla_1_pressed() -> void:
+	pass # Replace with function body.
