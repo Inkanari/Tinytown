7613c2c3708838b1d7ff15c9929659c37a198bc8
Inkanari <dkgames075@gmail.com>
2025-09-02 23:55:55 -0600

    exttras

--- comercio.gd
+++ comercio.gd
@@ -1,7 +1,15 @@
 extends GridContainer
 
 #productos
-var manzana = preload("res://manzana_item.tscn")
-var
+var manzana = preload("res://icons_textura/manzana.tres")
+var lingote_oro: Texture2D = preload("res://icons_textura/lingote_oro.tres")
 
-var productos = {}
+var productos = [
+	lingote_oro,
+	manzana
+]
+
+func _ready() -> void:
+	randomize()
+	var textura_random = productos.pick_random()
+	$casilla_1/TextureRect.texture = textura_random
